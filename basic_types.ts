2c43ea533815acdf95b12e6cf893a5b623b4e9c2
Roc Meseguer <roc.meseguer@upc.edu>
2024-09-22 18:39:54 +0200

    Types Updated

--- basic_types.ts
+++ basic_types.ts
@@ -1,13 +1,31 @@
+//
+// TS Types
+//
+
+// Primitive types are the most basic types
+let myName: string = "Aaron"; // strings
+let height: number = 1.78; // numbers: floting point numbers
+let isDone: boolean = true; // boolean
 
 let aCounter; // unkwnow (any) type
 let bCounter = 0; // number(inferred)
 let cCounter: number; //number
 let dCounter: number = 0; //number
 
-// Primitive types are the most basic types
-let myName: string = "Aaron"; // strings
-let height: number = 1.78; // numbers: floting point numbers
-let isDone: boolean = true; // boolean
+// Union Types
+let id: number | string;
+id = 10;
+id = "11";
+
+//  Type assertions
+let someValue: any = "Hello, TypeScript!";
+let strLength: number = (someValue as string).length;
+console.log(strLength); 
+
+let anotherValue: number | string = "Learning TypeScript";
+let anotherStrLength: number = (<string>anotherValue).length;
+console.log(anotherStrLength);  
+
 
 // Arrays of types
 const listOfNotes: number[] = [7, 10, 4];
@@ -25,8 +43,7 @@ let strNumTuple: [string, number];
 strNumTuple = ["Hello", 1000];
 
 
-// Any
-// all types in ts are subtypes of 'Any Type'
+// Any - all types in ts are subtypes of 'Any Type'
 let anyData: any = 1;
 anyData = true;
 anyData = "maybe a string instead";
@@ -35,19 +52,11 @@ let listOfAny: any[] = [1, true, "free"];
 listOfAny[1] = 100;
 
 
-// Union Types
-let id: number | string;
-id = 10;
-id = "11";
-console.log(id);
-
-
 // Functions
 
 function greets(name: string): string {
   return `Hello + ${name}`;
 }
-console.log(greets("Juan"));
 
 //optional parameter
 function addThree(x: number, y: number, z?: number): number {
@@ -57,8 +66,9 @@ function addThree(x: number, y: number, z?: number): number {
   return x + y;
 }
 
-// Objects
+// JSON Objects
 
+// type
 type Task = {
   id: string | number
   title: string
@@ -72,7 +82,7 @@ const task: Task = {
 }
 
 
-// Basic interface
+// interface
 interface Product {
   name: string;
   readonly price: number; // readonly is a modifier
@@ -86,3 +96,12 @@ const product: Product = {
 };
 
 
+// enums
+
+enum Status {
+  Success = "SUCCESS",
+  Failure = "FAILURE",
+  Pending = "PENDING"
+}
+
+let currentStatus: Status = Status.Success;
